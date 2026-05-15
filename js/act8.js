4879b5c6cdf576333c0da75c953e705eacd8b87f
karelgajudo23-netizen <karelgajudo23@gmail.com>
2026-05-15 13:35:12 +0845

    Add files via upload

---
+++ js/act8.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+function getInnerText() {
+  alert(document.getElementById("example").innerText);
+}
+
+function getInnerHTML() {
+  alert(document.getElementById("example").innerHTML);
+}
+
+function getTextContent() {
+  alert(document.getElementById("example").textContent);
+}
